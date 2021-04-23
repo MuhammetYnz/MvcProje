d480603c3c08d8946e5dbf3f3d73c10d66d0e510
Muhammet YANAZ <46990730+MuhammetYnz@users.noreply.github.com>
2021-04-24 00:15:12 +0300

    MvcProje(1/4)

---
+++ MvcProjeUI/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+   public class CategoryManager:GenericRepository<Category>
+    {
+        //GenericRepository<Category> repo = new GenericRepository<Category>();
+
+    }
+}
---
+++ MvcProjeUI/DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+   public interface ICategoryDal:IRepository<Category>
+    {
+        //Bu tarz kullanım  her sınıf tek tek interface oluşturulmasına neden olacağından büyük projelerde çok fazla bunun gibi gereksiz interface olmasına neden olacaktır.Bu yüzden bir tane IRepository üzerinden bu sorundan kurtulabiliriz.Bu sınıf örnek olarak tutulacak kullanılmayacaktır !!!!!
+
+        //CRUD
+        //Type Name();
+        //List<Category> List();
+
+        //void Insert(Category c);
+
+        //void Update(Category c);
+
+        //void Delete(Category c);
+    }
+}
