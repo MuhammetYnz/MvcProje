b65b850a79dabb1409bb77ac5aa26ce3f83a7322
Muhammet YANAZ <46990730+MuhammetYnz@users.noreply.github.com>
2021-05-14 15:12:30 +0300

    MvcProje(2/4)

--- MvcProjeUI/DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
+++ MvcProjeUI/DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,10 +14,16 @@ namespace DataAccessLayer.Concrete.Repositories
     {
         Context context = new Context();
         DbSet<Category> _object;
+      
         public void Delete(Category c)
         {
             _object.Remove(c);
             context.SaveChanges();
+        }
+
+        public Category Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
         }
 
         public void Insert(Category c)
@@ -28,6 +35,11 @@ namespace DataAccessLayer.Concrete.Repositories
         public List<Category> List()
         {
             return _object.ToList();
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
         }
 
         public void Update(Category c)
---
+++ MvcProjeUI/MvcProjeUI/Controllers/IstatistikController.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeUI.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        Context context = new Context();
+
+        // GET: Istatistik
+        public ActionResult Index()
+        {
+            ViewBag.KategoriToplami =context.Categories.Count().ToString();
+
+            ViewBag.Yazilim = context.Headings.Count(x => x.Category.CategoryName == "Yazılım").ToString(); 
+
+            ViewBag.Yazar = context.Writers.Count(x => x.WriterName.Contains("a")).ToString();
+
+            ViewBag.BasligiEnCokKategori = context.Headings.Select(x => x.Category.CategoryName).Max().ToString();
+
+            
+            int truee = context.Categories.Count(x => x.CategoryStatus == true);
+            int falsee = context.Categories.Count(x => x.CategoryStatus == false);
+            ViewBag.KategoriDurum = truee - falsee;
+
+
+            return View();
+        }
+    }
+}
